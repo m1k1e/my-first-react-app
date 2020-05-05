b782d55f1af6e7da592c7f3f2f2468eac0cc5d97
m1k1e <mikle.fedorenko@gmail.com>
2020-05-05 22:55:32 +0300

    Add new feature. Input text article.

--- src/index.js
+++ src/index.js
@@ -1,27 +1,6 @@
 import React from 'react'
 import {render} from 'react-dom'
+import App from './components/App'
 
-function Article() {
-  const body = <section>body</section>
-  return (
-     <div>
-      <h2>title</h2>
-      {body}
-      <h3>
-        	create date: {(new Date).toDateString()}
-      </h3>
-      </div>
-  )
-}
-
-
-function App() {
-  return (
-    <div>
-      <h1>App Name</h1>
-      <Article />
-    </div>
-  )
-}
 
 render(<App/>, document.getElementById('root'))
